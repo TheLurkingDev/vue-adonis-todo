b633f43958f19fc2e7b46cd87e1d4b391e4341d8
TheLurkingDev <thelurkingdev@gmail.com>
2018-07-03 11:52:53 -0400

    registration / login

--- server/start/routes.js
+++ server/start/routes.js
@@ -16,5 +16,13 @@
 const Route = use('Route')
 
 Route.get('/', ({ request }) => {
-  return { greeting: 'Hello world in JSON' }
+  return { greeting: 'Hello world in JSON' };
+});
+
+Route.group(() => {
+  Route.post('auth/register', 'UserController.register');
+  Route.post('auth/login', 'UserController.login');
 })
+.prefix('api');
+
+
