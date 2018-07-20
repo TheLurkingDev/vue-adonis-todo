e17a7468e97e7abc57ba11ac5573f91d81d48346
TheLurkingDev <thelurkingdev@gmail.com>
2018-07-19 23:27:05 -0400

    edit and delete project

--- client/src/components/Projects.vue
+++ client/src/components/Projects.vue
@@ -1,14 +1,47 @@
 <template>
     <Panel title="Projects">
-        <div v-for="project in projects" :key="project.id">
-            {{ project.title }}
+        <div v-for="project in projects" :key="project.id" class="project mt-2">
+            <v-layout row wrap>
+                <v-flex xs9 class="text-xs-left">
+                    <span v-if="!project.isEditMode">
+                        {{ project.title }}
+                    </span>
+                    <v-text-field
+                        autofocus
+                        v-if="project.isEditMode"
+                        :value="project.title"
+                        @keyup.enter="saveProject(project)"
+                        @input="setProjectTitle({ project, title: $event})">
+                    </v-text-field>
+                </v-flex>
+                <v-flex xs3>
+                    <v-icon
+                        class="edit-icon"
+                        @click="setEditMode(project)"
+                        v-if="!project.isEditMode">
+                        edit
+                    </v-icon>
+                    <v-icon
+                        class="edit-icon"
+                        @click="saveProject(project)"
+                        v-if="project.isEditMode">
+                        check
+                    </v-icon>
+                    <v-icon
+                        class="edit-icon"
+                        @click="deleteProject(project)">
+                        delete
+                    </v-icon>
+                </v-flex>
+            </v-layout>
         </div>
-        <v-layout row wrap>
+        <v-layout row wrap class="mt-4">
             <v-flex xs8>
                 <v-text-field
                     placeholder="Project name..."
                     :value = "newProjectName"
-                    @input="setNewProjectName">
+                    @input="setNewProjectName"
+                    @keyup.enter="createProject">
                 </v-text-field>
             </v-flex>
             <v-flex xs4>
@@ -17,7 +50,9 @@
                     dark
                     class="mt-2"
                     color="green">
-                    <v-icon class="mr-2">add_circle</v-icon>
+                    <v-icon class="mr-2">
+                        add_circle
+                    </v-icon>
                     Create
                 </v-btn>
             </v-flex>
@@ -29,6 +64,9 @@
 import { mapMutations, mapState, mapActions } from 'vuex';
 
 export default {
+    mounted() {
+        this.fetchProjects();
+    },
     computed: {
         ...mapState('projects', [
             'newProjectName',
@@ -38,14 +76,28 @@ export default {
     methods: {
         ...mapMutations('projects', [
             'setNewProjectName',
+            'setEditMode',            
+            'setProjectTitle',
         ]),
         ...mapActions('projects', [
             'createProject',
+            'fetchProjects',
+            'saveProject',
+            'deleteProject',
         ]),
     },
 };
 </script>
 
 <style>
-
+    .project {
+        font-size: 20px;
+    }
+    .icon {
+        cursor: pointer;
+    }
+    .edit-icon:hover {
+        color: green;
+        border: 1px solid green;
+    }
 </style>
