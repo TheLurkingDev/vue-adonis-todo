191e93dcc59bb189254543c6c5c66c1f7ae86ba1
TheLurkingDev <thelurkingdev@gmail.com>
2018-07-21 15:56:34 -0400

    abstracted logic for creating and editing records into separate, reusable components

--- client/src/components/Projects.vue
+++ client/src/components/Projects.vue
@@ -1,71 +1,36 @@
 <template>
     <Panel title="Projects">
         <div v-for="project in projects" :key="project.id" class="project mt-2">
-            <v-layout row wrap>
-                <v-flex xs9 class="text-xs-left">
-                    <span v-if="!project.isEditMode">
-                        {{ project.title }}
-                    </span>
-                    <v-text-field
-                        autofocus
-                        v-if="project.isEditMode"
-                        :value="project.title"
-                        @keyup.enter="saveProject(project)"
-                        @input="setProjectTitle({ project, title: $event})">
-                    </v-text-field>
-                </v-flex>
-                <v-flex xs3>
-                    <v-icon
-                        class="edit-icon"
-                        @click="setEditMode(project)"
-                        v-if="!project.isEditMode">
-                        edit
-                    </v-icon>
-                    <v-icon
-                        class="edit-icon"
-                        @click="saveProject(project)"
-                        v-if="project.isEditMode">
-                        check
-                    </v-icon>
-                    <v-icon
-                        class="edit-icon"
-                        @click="deleteProject(project)">
-                        delete
-                    </v-icon>
-                </v-flex>
-            </v-layout>
+            <EditableRecord 
+                :isEditMode="project.isEditMode"
+                :title="project.title"
+                @onInput="setProjectTitle({ project, title: $event })"
+                @onEdit="setEditMode(project)"
+                @onSave="saveProject(project)"
+                @onDelete="deleteProject(project)"
+            />
         </div>
-        <v-layout row wrap class="mt-4">
-            <v-flex xs8>
-                <v-text-field
-                    placeholder="Project name..."
-                    :value = "newProjectName"
-                    @input="setNewProjectName"
-                    @keyup.enter="createProject">
-                </v-text-field>
-            </v-flex>
-            <v-flex xs4>
-                <v-btn
-                    @click="createProject"
-                    dark
-                    class="mt-2"
-                    color="green">
-                    <v-icon class="mr-2">
-                        add_circle
-                    </v-icon>
-                    Create
-                </v-btn>
-            </v-flex>
-        </v-layout>
+        <CreateRecord 
+            placeholder="Project name..."
+            @onInput="setNewProjectName"
+            :value="newProjectName"
+            @create="createProject"
+        />
     </Panel>
 </template>
 
 <script>
 import { mapMutations, mapState, mapActions } from 'vuex';
+import CreateRecord from '@/components/CreateRecord.vue';
+import EditableRecord from '@/components/EditableRecord.vue';
 
 export default {
     mounted() {
         this.fetchProjects();
+    },
+    components: {
+        CreateRecord,
+        EditableRecord,
     },
     computed: {
         ...mapState('projects', [
